4a2966385a31d0f29ebe50cc689c24f09b596649
musthafamum <musthafamumpang@gmail.com>
2019-04-09 23:36:06 +0530

    moved json parser to main

--- ipl_bot.py
+++ ipl_bot.py
@@ -1,8 +1,30 @@
 import telegram
 import threading
 import time
-from json_parser import if_six_hit
+import json
+import urllib.request
 
+match_json = {}
+
+privious_over_details = {}
+prevoius_over_last_run = {}
+
+def if_six_hit():
+    if_six_hit_bool = False
+    with urllib.request.urlopen("https://www.cricbuzz.com/match-api/livematches.json") as url:
+        match_json = json.loads(url.read().decode())
+    for match_id,match in match_json['matches'].items():
+        if match['series']['name'] == "Indian Premier League 2019":
+            if 'score' in match:
+                prev_over = match['score']['prev_overs']
+                if not match_id in  privious_over_details:
+                    prevoius_over_last_run[match_id] = prev_over[-2:]
+                    privious_over_details[match_id] = prev_over
+                else:
+                    if prev_over[-2:] == "1" and len(privious_over_details[match_id]) != len(prev_over):
+                        privious_over_details[match_id] = prev_over
+                        if_six_hit_bool = True
+    return if_six_hit_bool
 
 
 
@@ -12,22 +34,24 @@ from json_parser import if_six_hit
 
 subscriber_list = []
 bot = telegram.Bot(token="893131928:AAFtc2fiFDBn5tRdmp17RunwyLozMOqMw7Y")
-
+html = ""
 
 
 def broadcaster():
     while True:
-        time.sleep(30)
+        time.sleep(4)
+        print("if six::::"+str(if_six_hit()))
         if if_six_hit():
             for subscriber in subscriber_list:
-               bot.send_message(chat_id=subscriber, text="I'm sorry Dave I'm afraid I can't do that.") 
-
+            #    bot.send_message(chat_id=subscriber, text="I'm sorry Dave I'm afraid I can't do that.",parse_mode=telegram.ParseMode.HTML) 
+                print("hi")
 broadcaster_thread = threading.Thread(target=broadcaster, args=())
 broadcaster_thread.start()
 
 while True:
     updates = bot.get_updates()
-    chat_id = bot.get_updates()[-1].message.chat_id
-    if not chat_id in subscriber_list:
-        subscriber_list.append(chat_id)
+    if updates:
+        chat_id = bot.get_updates()[-1].message.chat_id
+        if not chat_id in subscriber_list:
+            subscriber_list.append(chat_id)
         
--- json_parser.py
+++ json_parser.py
@@ -3,34 +3,30 @@ import urllib.request
 
 match_json = {}
 
-#This dictionary stores the following
-# match_id : { old_batsman_1_id,old_batsman_2_id,old_batsman_1_sixes,old_batsman_2_sixes,new_batsman_1_id,new_batsman_2_id,new_batsman_1_sixes,new_batsman_2_sixes }
-matches_and_batsman = {}
+privious_over_details = {}
+prevoius_over_last_run = {}
 
 
 def if_six_hit():
+    if_six_hit_bool = False
     with urllib.request.urlopen("https://www.cricbuzz.com/match-api/livematches.json") as url:
         match_json = json.loads(url.read().decode())
     for match_id,match in match_json['matches'].items():
-        if match['series']['name'] == "CSA T20 Challenge 2019":
-            if not match_id in matches_and_batsman:
-                #matches_and_batsman[match_id] = { old_batsman_1_id,old_batsman_2_id,old_batsman_1_sixes,old_batsman_2_sixes,new_batsman_1_id,new_batsman_2_id,new_batsman_1_sixes,new_batsman_2_sixes}
-                pass
+        if match['series']['name'] == "Indian Premier League 2019":
             if 'score' in match:
-                batsman1 = match['score']['batsman'][0]
-                batsman2 = match['score']['batsman'][1]
-
-                current_batsamn_1 = batsman1['id']
-                current_batsamn_2 = batsman2['id']
-
-
-                # if batsman1['id'] != current_batsamn_1:
-                #     print("New batsamn 1")
-                print(batsman1)
-                print(batsman2)
-    return True
+                prev_over = match['score']['prev_overs']
+                if not match_id in  privious_over_details:
+                    prevoius_over_last_run[match_id] = prev_over[-2:]
+                    privious_over_details[match_id] = prev_over
+                else:
+                    if prev_over[-2:] == "1" and len(privious_over_details[match_id]) != len(prev_over):
+                        if_six_hit_bool = True
 
 
-if_six_hit()
+
+    return if_six_hit_bool
+
+
+print(if_six_hit())
 
 
